85e95d9b69132b3014667e49b4faa03febf14ca5
MaDHouSe79 <madhouse1979@gmail.com>
2023-10-08 02:01:43 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
---[[ ===================================================== ]]--
---[[          QBCore Housenames Script by MaDHouSe         ]]--
---[[ ===================================================== ]]--
-
-Config                       = {}
-Config.HouseNameOffSet       = 1.0
+--[[ ===================================================== ]] --
+--[[            MH Housenames Script by MaDHouSe           ]] --
+--[[ ===================================================== ]] --
+Config = {}
+Config.HouseNameOffSet = 1.0
 Config.DrawHouseNameDistance = 10
-Config.DrawButtonDictance    = 3
-Config.InteractButton        = 38 -- E
-Config.ViewHouseButton       = 23 -- F
-Config.TextScale             = 0.04
-Config.JobLabel              = "Realestate"
+Config.DrawButtonDictance = 3
+Config.InteractButton = 38 -- E
+Config.ViewHouseButton = 23 -- F
+Config.TextScale = 0.04
+Config.JobLabel = "Realestate"
+Config.ShowBlips = true
